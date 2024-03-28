ea2e40f412d5fce780e0d8e21cd095306b3b5892
Porsi2024 <gabrielporsi2@gmail.com>
2024-03-28 11:43:29 -0300

    segundo do js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
-
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
 }
-
 const pessoasInput = document.querySelector("#pessoas")
 pessoasInput.addEventListener("input", receberQuantidadePessoas)
-
 function receberQuantidadePessoas(evento) {
     const paragrafoErro = document.querySelector(".pessoas #erro")
     const divErro = document.querySelector(".pessoas .input-box")
-
     if(evento.target.value ===  "0") {
         paragrafoErro.style.display = "block"
         divErro.setAttribute("id", "erro-div")
@@ -23,4 +20,14 @@ function receberQuantidadePessoas(evento) {
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+}
+
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao =>  {
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+function receberPorcentagem(evento) {
+   console.log(evento.target.value)
 }
